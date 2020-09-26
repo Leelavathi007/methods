b1f964d4fe25068b39c85089f44472ed59b5ab25
Leelavathi.M <leelavathip007@gmail.com>
2020-09-26 19:35:36 +0530

    methods

---
+++ src/methods.java
@@ -0,0 +1,19 @@
+public class methods {
+    public void method1(int a) {
+        return;
+    }
+
+    public void method2(float b){
+        System.out.println("b:"+b);
+        return;
+    }
+    public void method3(int a,float b){
+        double sum = a+b;
+        double square = (a+b)*(a+b);
+        System.out.println("Sum of two numbers is:"+sum);
+        System.out.println("square of the sum of two numbers:"+square);
+        return;
+    }
+
+
+}
